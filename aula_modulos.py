442e3afb8f2dcb3bfe31b728275f6b3126fdae1a
evertoncam7 <everton.camargo.cam7@gmail.com>
2021-04-03 19:20:21 -0300

    Aula de Python

---
+++ aula_modulos.py
@@ -0,0 +1,19 @@
+from math import sqrt
+
+nome = str(input("digite seu nome..."))
+num = 9
+raiz = sqrt(num)
+
+print('O seu nome é {}, não é mesmo?'.format(nome))
+print('A raiz quadrada de {} é {}'.format(num, raiz))
+
+nome = str(input('digite o seu nome aqui'))
+idade = int(input('digite a sua idade aqui'))
+
+if idade == 34:
+    print('Está ficando velho em {}'.format(nome))
+else:
+    print('Eu acho que está mentindo a sua idade rsrsr')
+print('Bricadeira {}'.format(nome))
+
+
